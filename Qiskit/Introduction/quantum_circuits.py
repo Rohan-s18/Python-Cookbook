88cca47a1d2e4a9e87d29b80bd2d7aae9a6ebc7d
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-29 17:21:17 -0500

    Added code for simple quantum circuit using Qiskit

---
+++ Qiskit/Introduction/quantum_circuits.py
@@ -0,0 +1,30 @@
+"""
+Author: Rohan Singh
+1/29/23
+This Python module contains source code simple Quantum Circuits
+"""
+
+#  Imports 
+from qiskit import QuantumCircuit, assemble, Aer
+from qiskit.visualization import plot_histogram
+
+#  Creating a Quantum Circuit with 8 qubits (8 outputs)
+qc_output = QuantumCircuit(8)
+
+#  Adding measurements to each of the qubits
+qc_output.measure_all()
+
+#  The qubits are all intialized to 0, so the output will be all zeros
+print(qc_output.draw(initial_state=True))
+
+
+
+sim = Aer.get_backend('aer_simulator') 
+result = sim.run(qc_output).result()
+counts = result.get_counts()
+
+#  Printing the counts
+print(counts)
+plot_histogram(counts)
+
+
