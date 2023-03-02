305372736207d06ed88b69ff50700bc3d9a91a40
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-02 15:54:25 -0500

    Added template for numpy

---
+++ Numpy/Arrays/products.py
@@ -0,0 +1,15 @@
+"""
+Author: Rohan Singh
+3/2/2023
+Python Module to demonstrate various vector products in python using numpy
+"""
+
+#  Imports
+
+
+#  Main function
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ Numpy/README.md
@@ -0,0 +1,3 @@
+# Numpy
+## Author: Rohan Singh and Rachel Tjarksen
+This directory contains source code for Python's numpy library.
