24e7764a0da4e62ffefd58bc9d9882b14ff67b3e
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-02 16:31:59 -0500

    Added basic numpy stuff

--- Numpy/Arrays/products.py
+++ Numpy/Arrays/products.py
@@ -5,11 +5,25 @@ Python Module to demonstrate various vector products in python using numpy
 """
 
 #  Imports
+import numpy as np
+import random as rand
 
+def create_random_array(n,min,max)->np.ndarray:
+    temp = np.zeros(10)
+    for i in range(0,n,1):
+        temp[i] = (min+int((max-min)*rand.random()))
+    return temp
 
 #  Main function
 def main():
-    pass
+    x = create_random_array(10,-100,100)
+    y = create_random_array(10,-100,100)
+    print("The vector x is: ", x)
+    print("The vector y is: ", y)
+
+    #dot-product
+    print("The dot prodcuct of x and y is: ",np.dot(x,y))
+
 
 if __name__ == "__main__":
     main()
