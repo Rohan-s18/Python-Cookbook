481f95c90511706689b673a1e8e1d56b6e091483
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-03 12:14:03 -0500

    Added Code for vector calculus

--- Numpy/Arrays/calculus.py
+++ Numpy/Arrays/calculus.py
@@ -25,12 +25,40 @@ def create_random_vector(n, min, max)->list:
     return temp
 
 
+#  Function to create random infinitesimal elements
+def create_random_element(n)->np.ndarray:
+    temp = np.zeros(10)
+    for i in range(0,n,1):
+        temp[i] = (rand.random()*0.1)
+    return temp
+
+
 #  Main function
 def main():
-    pass
+
+    print("\n")
+
+    #vector derivative for constant 'dx'
+    dx = 0.1
+    y = create_random_array(10,-100,100)
+    print("dx is: ",dx)
+    print("y is: ",y)
+    print("dy/dx is: ",np.diff(y)/dx)
+
+    print("\n")
+
+    #vector derivative for non-constant 'dx'
+    dx = create_random_element(10)
+    y = create_random_array(10,-100,100)
+    print("dx is: ",dx)
+    print("y is: ",y)
+    print("dy/dx is: ",np.diff(y)/np.diff(dx))
+
+    print("\n")
+
+
 
 
 if __name__ == "__main__":
     main()
 
-    
