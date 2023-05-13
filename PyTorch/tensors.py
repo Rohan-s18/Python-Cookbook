10d966527f0e710257a009e1f7baed01807f182c
Rohan Singh <rohan.b.singh54@gmail.com>
2023-05-12 22:15:07 -0400

    Finished Tensor Code

--- PyTorch/tensors.py
+++ PyTorch/tensors.py
@@ -10,13 +10,32 @@ import torch
 
 #  Function to print tensor properties
 def get_props(tensor):
-    print(f'Shape of {tensor} is {tensor.shape} with dimensions {tensor.dim()}\n\n')
+    print(f'Shape of {tensor} is {tensor.shape} with dimensions {tensor.dim()}\nThe Tensor is stored in {tensor.device}\n\n')
 
 
 #  Function to change the dimensions of a tensor
 def change_dim(tensor, n):
     temp = tensor.unsqueeze(n)
     return temp
+
+
+#  Function to return a zero/ones/rand tensors of a given shape
+def make_tensors(shape):
+
+    #Creating the tensors
+    rand_tensor = torch.rand(shape)
+    ones_tensor = torch.ones(shape)
+    zeros_tensor = torch.zeros(shape)
+
+    return rand_tensor, ones_tensor, zeros_tensor
+
+
+#  Function to represent operations on Tensors
+def multiply(tensor_a, tensor_b):
+    
+    z = tensor_a.matmul(tensor_b)
+    return z
+
 
 
 #  Main Function
@@ -41,6 +60,9 @@ def main():
 
     get_props(two_d_tensor_2)
     get_props(two_d_tensor_3)
+
+    z = multiply(two_d_tensor_2, two_d_tensor_3)
+    get_props(z)
 
 
 if __name__ == "__main__":
